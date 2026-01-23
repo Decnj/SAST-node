dbc14e28d0f90291a6adfcdaaf766935e90f6298
Decnj <nnamdi_njemanze@yahoo.com>
2026-01-23 18:05:56 +0100

    Setup basic express server in app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+#Import Express
 const express = require('express');
+
+#Create express instance
 const app = express();
 
+#Define routes
 app.get('/', (req, res) => res.send('Express server"));
 
+#Start express server
 app.listen(3000, () => console.log('Server is running on http://localhost:3000));
